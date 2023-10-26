fb60be47b8cab4c3c723e487f290cc54d20f986e
GopalAror <punjabijanu586@gmail.com>
2023-10-26 16:10:05 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,110 @@
+let mobileView = document.querySelector(".mobileView")
+let menu = document.querySelector("#menu")
+let span1 = document.querySelector(".span1")
+let logo = document.querySelector(".logo-show")
+let crs = document.querySelector(".crs")
+let cross = document.querySelector(".cross")
+
+menu.addEventListener("click", function () {
+  mobileView.classList.toggle("show");
+  document.querySelector("body").classList.toggle("overflow-hidden")
+  logo.classList.toggle("d-none")
+  span1.classList.toggle("d-none")
+  cross.classList.add("d-block")
+  cross.classList.toggle("d-none")
+});
+// backto top//
+function scrollBtn() {
+  window.scrollTo(0, 0);
+}
+window.addEventListener("scroll", function () {
+  let topbtn = document.getElementById("topbtn");
+  if (window.scrollY > 500) {
+      topbtn.style.display = 'block';
+  }
+  else{
+      topbtn.style.display = 'none';
+  }
+})
+// preloder
+setTimeout(() => {
+  document.getElementById("preloder").classList.add("d-none");
+  document.body.classList.remove("overflow-hidden");
+}, 2000);
+// --------------------------------------------slider-1-----------------------------------------//
+
+$('.slidr1').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  prevArrow: false,
+  nextArrow: false,
+  responsive: [
+    {
+      breakpoint: 9999,
+      settings: "unslick"
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+// ---------------------------------------------------slider-2--------------------------------------///
+$('.slidr2').slick({
+  centerMode: true,
+  centerPadding: '300px',
+  slidesToShow: 1,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: "unslick"
+    }
+  ]
+});
+//----------------------------------------------------- slider-3-------------------------------------//
+
+$('.slider3').slick({
+  centerMode: true,
+  centerPadding: '250px',
+  slidesToShow: 1,
+  prevArrow:false,
+  nextArrow:false, 
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '100px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '10px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+
